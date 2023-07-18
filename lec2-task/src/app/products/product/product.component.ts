07fbb250153f9b11a1de5d4af3591a7d60c5e7b5
Magdy Mohammed Hanafy <mm312220@gmail.com>
2023-07-18 09:26:34 +0200

    First

---
+++ lec2-task/src/app/products/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, Injectable, Input } from '@angular/core';
+import { Product } from '../models/productModel';
+
+@Component({
+  selector: 'productItem-root',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductItemComponent {
+  @Input() productItem: Product;
+  @Input() products!: Product[];
+  constructor() {
+    this.productItem = new Product();
+  }
+
+  largerThan100(product: Product) {
+    return product.priceProduct > 100;
+  }
+}
